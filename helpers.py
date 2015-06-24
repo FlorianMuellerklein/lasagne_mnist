3b8063e48a038e6359153f29120bd7813ebca963
Florian <f.muellerklein@gmail.com>
2015-06-24 07:04:17 -0400

    first commit

---
+++ helpers.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+from random import randint, uniform
+
+import seaborn as sns
+from matplotlib import pyplot
+from skimage.io import imshow
+from skimage.util import crop
+from skimage import transform, filters, exposure
+
+PIXELS = 28
+imageSize = PIXELS * PIXELS
+num_features = imageSize
+
+def fast_warp(img, tf, output_shape, mode='nearest'):
+    return transform._warps_cy._warp_fast(img, tf.params, output_shape=output_shape, mode=mode)
+
+def batch_iterator(data, y, batchsize, train):
+    '''
+    Data augmentation batch iterator for feeding images into CNN.
+    This example will randomly rotate all images in a given batch between -10 and 10 degrees
+    and to random translations between -5 and 5 pixels in all directions.
+    Random zooms between 1 and 1.3.
+    Random shearing between -20 and 20 degrees.
+    Randomly applies sobel edge detector to 1/4th of the images in each batch.
+    Randomly inverts 1/2 of the images in each batch.
+    '''
+
+    n_samples = data.shape[0]
+    loss = []
+    for i in range((n_samples + batchsize -1) // batchsize):
+        sl = slice(i * batchsize, (i + 1) * batchsize)
+        X_batch = data[sl]
+        y_batch = y[sl]
+
+        # set empty copy to hold augmented images so that we don't overwrite
+        X_batch_aug = np.empty(shape = (X_batch.shape[0], 1, PIXELS, PIXELS), dtype = 'float32')
+
+        # random rotations betweein -8 and 8 degrees
+        dorotate = randint(-5,5)
+
+        # random translations
+        trans_1 = randint(-3,3)
+        trans_2 = randint(-3,3)
+
+        # random zooms
+        zoom = uniform(1, 1.2)
+
+        # shearing
+        shear_deg = uniform(-10, 10)
+
+        # set the transform parameters for skimage.transform.warp
+        # have to shift to center and then shift back after transformation otherwise
+        # rotations will make image go out of frame
+        center_shift   = np.array((PIXELS, PIXELS)) / 2. - 0.5
+        tform_center   = transform.SimilarityTransform(translation=-center_shift)
+        tform_uncenter = transform.SimilarityTransform(translation=center_shift)
+
+        tform_aug = transform.AffineTransform(rotation = np.deg2rad(dorotate),
+                                              scale =(1/zoom, 1/zoom),
+                                              shear = np.deg2rad(shear_deg),
+                                              translation = (trans_1, trans_2))
+
+        tform = tform_center + tform_aug + tform_uncenter
+
+        # images in the batch do the augmentation
+        for j in range(X_batch.shape[0]):
+
+            X_batch_aug[j][0] = fast_warp(X_batch[j][0], tform,
+                                          output_shape = (PIXELS, PIXELS))
+
+        # use sobel edge detector filter on one quarter of the images
+        indices_sobel = np.random.choice(X_batch_aug.shape[0], X_batch_aug.shape[0] / 4, replace = False)
+        for k in indices_sobel:
+            img = X_batch_aug[k][0]
+            X_batch_aug[k][0] = filters.sobel(img)
+
+        # fit model on each batch
+        loss.append(train(X_batch_aug, y_batch))
+
+    return np.mean(loss)
+
+def one_hot(x,n):
+	if type(x) == list:
+		x = np.array(x)
+	x = x.flatten()
+	o_h = np.zeros((len(x),n))
+	o_h[np.arange(len(x)),x] = 1
+	return o_h
+
+def load_data_cv(train_path):
+
+    print('read data')
+    # reading training data
+    training = pd.read_csv(train_path)
+    training = training.values
+
+    # split training labels and pre-process them
+    training_targets = training[:,num_features]
+    training_targets = training_targets.astype('int32')
+    print training_targets.shape
+    training_targets = one_hot(training_targets, 10)
+    print training_targets.shape
+
+    # split training inputs and scale data 0 to 1
+    training_inputs = training[:,0:num_features].astype('float32')
+    training_inputs = training_inputs / 255.
+    print training_inputs.shape
+
+    # train test split
+    x_train, x_test, y_train, y_test = train_test_split(training_inputs, training_targets, test_size = 0.1)
+
+    print 'train size:', x_train.shape[0], 'eval size:', x_test.shape[0]
+
+    # reshaping training and testing data so it can be feed to convolutional layers
+    x_train = x_train.reshape(x_train.shape[0], 1, PIXELS, PIXELS)
+    x_test = x_test.reshape(x_test.shape[0], 1, PIXELS, PIXELS)
+
+    return x_train, x_test, y_train, y_test
+
+def load_test_data(test_path):
+    print('read data')
+    # reading training data
+    testing = pd.read_csv(test_path)
+    testing = testing.values
+
+    # split training inputs and scale data 0 to 1
+    testing_inputs = testing[:,0:num_features].astype('float32')
+    testing_inputs = testing_inputs / 255.
+    print testing_inputs.shape
+
+    # reshaping training and testing data so it can be feed to convolutional layers
+    testing_inputs = testing_inputs.reshape(testing_inputs.shape[0], 1, PIXELS, PIXELS)
+
+    return testing_inputs
---
+++ mnist_cnn.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+
+import theano
+from theano import tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+from theano.tensor.nnet.conv import conv2d
+from theano.tensor.signal.downsample import max_pool_2d
+
+import lasagne
+from lasagne.nonlinearities import rectify, softmax, very_leaky_rectify
+from lasagne.updates import nesterov_momentum
+from lasagne.layers import InputLayer, MaxPool2DLayer, Conv2DLayer, DenseLayer, DropoutLayer, helper
+
+from random import randint, uniform
+from helpers import batch_iterator, load_data_cv, load_test_data
+
+import seaborn as sns
+from matplotlib import pyplot
+
+BATCHSIZE = 32
+PIXELS = 28
+imageSize = PIXELS * PIXELS
+num_features = imageSize
+
+srng = RandomStreams()
+
+# set up functions needed to train the network
+def floatX(X):
+    return np.asarray(X, dtype=theano.config.floatX)
+
+
+def lasagne_model():
+    l_in = InputLayer(shape=(None, 1, 28, 28))
+
+    l_conv1 = Conv2DLayer(l_in, num_filters = 32, filter_size=(3,3), nonlinearity=rectify)
+    l_conv1b = Conv2DLayer(l_conv1, num_filters = 32, filter_size=(3,3), nonlinearity=rectify)
+    l_pool1 = MaxPool2DLayer(l_conv1b, pool_size=(2,2))
+    l_dropout1 = DropoutLayer(l_pool1, p=0.1)
+
+    l_conv2 = Conv2DLayer(l_pool1, num_filters = 64, filter_size=(3,3), nonlinearity=rectify)
+    l_conv2b = Conv2DLayer(l_conv2, num_filters = 64, filter_size=(3,3), nonlinearity=rectify)
+    l_pool2 = MaxPool2DLayer(l_conv2b, pool_size=(2,2))
+    l_dropout2 = DropoutLayer(l_pool2, p=0.2)
+
+    l_hidden3 = DenseLayer(l_dropout2, num_units = 512, nonlinearity=rectify)
+    l_dropout3 = DropoutLayer(l_hidden3, p=0.5)
+
+    l_out = DenseLayer(l_dropout3, num_units=10, nonlinearity=softmax)
+
+    return l_out
+
+def main():
+    # load the training and testing data sets
+    train_X, test_X, train_y, test_y = load_data_cv('data/train.csv')
+
+    X = T.ftensor4()
+    Y = T.fmatrix()
+
+    output_layer = lasagne_model()
+    output = helper.get_output(output_layer, X)
+
+    loss_train = T.mean(lasagne.objectives.categorical_crossentropy(output, Y))
+
+    output_valid = helper.get_output(output_layer, X, deterministic=True)
+    loss_valid = T.mean(lasagne.objectives.categorical_crossentropy(output_valid, Y))
+
+    pred = T.argmax(output, axis=1)
+    pred_valid = T.argmax(output_valid, axis=1)
+
+    params = helper.get_all_params(output_layer)
+    updates = nesterov_momentum(loss_train, params, learning_rate=0.005, momentum=0.9)
+
+    train = theano.function(inputs=[X, Y], outputs=loss_train, updates=updates, allow_input_downcast=True)
+    predict_valid = theano.function(inputs=[X], outputs=pred_valid, allow_input_downcast=True)
+
+    for i in range(50):
+        loss_train = batch_iterator(train_X, train_y, BATCHSIZE, train)
+        print 'iter:', i, '| Tloss:', loss_train, '| valid acc:', np.mean(np.argmax(test_y, axis=1) == predict_valid(test_X))
+
+    testing_inputs = load_test_data('data/test.csv')
+    predictions = []
+    for i in range((testing_inputs.shape[0] + BATCHSIZE -1) // BATCHSIZE):
+        sl = slice(i * BATCHSIZE, (i + 1) * BATCHSIZE)
+        X_batch = testing_inputs[sl]
+        predictions.extend(predict_valid(X_batch))
+    print len(predictions)
+    out = pd.read_csv('data/convnet_preds.csv')
+    out['Label'] = predictions
+    out.to_csv('preds/convnet_preds.csv', index = False)
+
+if __name__ == '__main__':
+    main()
